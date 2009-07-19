a6d3ead031121db40fb01e9d6157d73cc0a75529
troy <troy@4gsurf.(none)>
2009-07-19 14:19:57 -0700

    add bzip2

--- bootstrap-xorg-debian.sh
+++ bootstrap-xorg-debian.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aptitude install sudo rsync openssh-server git-core pkg-config build-essential autoconf libtool python
+aptitude install bzip2 sudo rsync openssh-server git-core pkg-config build-essential autoconf libtool python
