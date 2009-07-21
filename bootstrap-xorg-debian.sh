79ddb996e1beda295233ec5017e15990965058f4
troy <troy@eee.(none)>
2009-07-21 09:49:46 -0700

    modified:   ../bootstrap-xorg-debian.sh 	new file:   install-everything

--- bootstrap-xorg-debian.sh
+++ bootstrap-xorg-debian.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # July 2009 TDW
 # This are some Debian packages required the build X
-aptitude install bzip2 sudo rsync openssh-server git-core pkg-config build-essential autoconf libtool python mlocate xsltproc libexpat1-dev libfreetype6-dev libssl-dev
+aptitude install bzip2 sudo rsync openssh-server git-core pkg-config build-essential autoconf libtool python mlocate xsltproc libexpat1-dev libfreetype6-dev libssl-dev gettext
 # xsltproc is required by XCB
 # July 2009: libexpat1-dev: during building libGL => Expat required for DRI
 # libfreetype6-dev => required by libXfont
+# gettext => msgfmt ( jhbuild )
