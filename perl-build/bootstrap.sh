0e432619c64f1fd924bd70c9f731287364c073b1
troy <troy@eee.(none)>
2009-07-24 08:49:29 -0700

    new file

---
+++ perl-build/bootstrap.sh
@@ -0,0 +1,5 @@
+aptitude install libxml-simple-perl libxml-simpleobject-perl
+# The following NEW packages will be installed:
+#   libcompress-raw-zlib-perl{a} libcompress-zlib-perl{a} libfont-afm-perl{a} libhtml-format-perl{a} libhtml-parser-perl{a} libhtml-tagset-perl{a}
+#   libhtml-tree-perl{a} libio-compress-base-perl{a} libio-compress-zlib-perl{a} libmailtools-perl{a} liburi-perl{a} libwww-perl{a}
+#   libxml-namespacesupport-perl{a} libxml-parser-perl{a} libxml-sax-expat-perl{a} libxml-sax-perl{a} libxml-simple-perl
