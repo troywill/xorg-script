7d050373738eb74ca57c451605ef6d2d623dbd5a
root <root@eee-pc.(none)>
2009-07-24 00:45:20 -0700

    i

--- bootstrap-xorg-debian.sh
+++ bootstrap-xorg-debian.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # July 2009 TDW
 # This are some Debian packages required the build X
-aptitude install build-essential emacs22-nox git-core autoconf libtool bzip2 sudo rsync openssh-server pkg-config python mlocate xsltproc libexpat1-dev libfreetype6-dev libssl-dev gettext flex
+aptitude install build-essential emacs22-nox git-core autoconf libtool bzip2 sudo rsync openssh-server pkg-config python mlocate xsltproc libexpat1-dev libfreetype6-dev libssl-dev gettext bison flex intltool libglib2.0-dev
 # xsltproc is required by XCB
 # July 2009: libexpat1-dev: during building libGL => Expat required for DRI
 # libfreetype6-dev => required by libXfont
 # gettext => msgfmt ( jhbuild )
-# flex => needed by twm
+# bison, flex => needed by twm
+# intltoolize => needed by xkeyboard-config
+# xkeyboard-config => AM_GLIB_GNU_GETTEXT: command not found => libglib2.0-dev
 
