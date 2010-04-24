2c11add44524477712f91d9c437d4d433c139f02
Troy Will <troydwill@gmail.com>
2010-04-24 01:41:31 +0000

    modified:   build-xorg.cfg

--- perl-build-beta/data/build-list/generate-build-list.sh
+++ perl-build-beta/data/build-list/generate-build-list.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 jhbuild list xserver xf86-video-intel xf86-input-keyboard xkeyboard-config xf86-input-mouse xorg-apps > temp1
 jhbuild list xserver xf86-video-intel xf86-input-keyboard xkeyboard-config xf86-input-mouse xorg-apps xorg-fonts > temp2
-# manually remove meta modules xorg-protos and xorg-apps
+# manually remove meta modules xorg-protos, xorg-apps, and xorg-fonts
+
