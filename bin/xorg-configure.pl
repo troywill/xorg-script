b313dc4921a8c18e46b6f62e19c17ad6bd1db29b
Troy Will <troydwill@gmail.com>
2010-04-28 04:41:40 +0000

    new file:   ../bin/xorg-configure.pl

---
+++ bin/xorg-configure.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+
+# system("sudo X -configure &");
+# sleep 3;
+system("sudo killall X");
+system("sudo X -retro -config ~/xorg.conf.new &");
+sleep 6;
+system("sudo killall X");
